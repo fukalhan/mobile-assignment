3a8cc6d0c0c199993049c718930569880077ca6f
Hana Fukalova <hana.fukalova@quanti.cz>
2023-04-11 16:36:31 +0200

    REF: Change use case di

--- rocket-app/library/rocket-data/src/main/kotlin/cz/quanti/rocketapp/rocketdata/di/RocketDataModule.kt
+++ rocket-app/library/rocket-data/src/main/kotlin/cz/quanti/rocketapp/rocketdata/di/RocketDataModule.kt
@@ -5,13 +5,14 @@ import cz.quanti.rocketapp.rocketdata.domain.GetRocketDetailUseCase
 import cz.quanti.rocketapp.rocketdata.domain.GetRocketsUseCase
 import cz.quanti.rocketapp.rocketdata.domain.RocketRepository
 import cz.quanti.rocketapp.rocketdata.system.RetrofitBuilder
+import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val rocketDataModule = module {
     singleOf(::SpaceXRocketRepository) bind RocketRepository::class
-    singleOf(::GetRocketsUseCase)
-    singleOf(::GetRocketDetailUseCase)
+    factoryOf(::GetRocketsUseCase)
+    factoryOf(::GetRocketDetailUseCase)
     single { RetrofitBuilder.apiService }
 }

e7adb8c2f103d6aba27a3a31df6c466b037ae0e5
Hana Fukalova <hana.fukalova@quanti.cz>
2023-04-11 16:45:43 +0200

    REF: Fix images width alignment

--- rocket-app/feature/rocket-detail/src/main/kotlin/cz/quanti/rocketapp/rocketdetail/system/Photos.kt
+++ rocket-app/feature/rocket-detail/src/main/kotlin/cz/quanti/rocketapp/rocketdetail/system/Photos.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import coil.compose.AsyncImage
 import cz.quanti.rocketapp.design.system.RocketAppTheme
@@ -43,5 +45,8 @@ fun Photo(url: String) {
         contentDescription = "Rocket photo",
         modifier = Modifier
             .clip(RoundedCornerShape(RocketAppTheme.dimensions.roundCorners))
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        contentScale = ContentScale.FillWidth
     )
 }
